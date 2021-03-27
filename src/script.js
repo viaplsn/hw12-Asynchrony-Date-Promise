a67b8f34797c34379f57700479018903fdfc07f9
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-03-27 12:31:01 +0200

    getRandomChinese function added.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!")
+async function getRandomChinese(length) {
+    let counter = 0;
+    let result = "";
+    while (counter < length) {
+        await new Promise((resolve) => {
+            result += String.fromCharCode(Date.now()).slice(-5);
+            counter++;
+            setTimeout(() => {
+                resolve(result)
+            }, 50);
+        });
+    };
+    console.log(result);
+};
+getRandomChinese(4);
